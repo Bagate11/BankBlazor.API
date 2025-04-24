1ae11b57bd414f604611a4dafd04c362aab506b8
ermias <ermias03555@gmail.com>
2025-04-24 09:42:42 +0200

    Det här är min Account Class där jag har lagt in vad jag kommer behöva .

---
+++ BankBlazor.Client/Customer, Account, Transaction/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace BankBlazor.Client.Customer__Account__Transaction
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<Account> Accounts { get; set; } = new();
+    }
+}
---
+++ BankBlazor.Client/Customer, Account, Transaction/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Principal;
+
+namespace BankBlazor.Client.Customer__Account__Transaction
+{
+    public enum TransactionType { Deposit, Withdrawal, Transfer }
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public int? RelatedAccountId { get; set; }
+    }
+}
