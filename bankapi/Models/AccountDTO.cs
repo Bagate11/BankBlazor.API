613893875d681bdc5e1cfe729acb8641d60d2b9b
ermias <ermias03555@gmail.com>
2025-05-22 12:51:48 +0200

    Nu har jag gjort min sCroffold dör jag fick flera classer från samt det som ska va i dbcontexten samt har jag också skapat DTO class som ACCountdto som jag har tagit relevanta infromation som jag behöver för att bygga denna class  som  (public int AccountId { get; set; } ,  public decimal Balance { get; set; },      public string? AccountHolderName { get; set; }

---
+++ bankapi/Models/AccountDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace bankapi.Models
+{
+    public class AccountDTO
+    {
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+        public string? AccountHolderName { get; set; }
+    }
+}
---
+++ bankapi/Program.cs
@@ -0,0 +1,44 @@
+
+using BankBlazor.Client.DB;
+using BankBlazor.Client.DB.bankapi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace bankapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<BankContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankDatabase")));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
